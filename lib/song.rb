780b2684b897fcdaa9598f277d7a7512c589ad8d
Ruth Mesfun <ruth@flatironschool.com>
2017-09-17 10:47:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initializes(name, artist_name)
+    @name = name
+    @artist_name = name
+    @@all << self
+  end
+
   def self.create
     @@all << self
 
