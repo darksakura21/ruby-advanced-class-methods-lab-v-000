f28b1a1a8405681bf2def4ab3ebb7c3dfa4b2e92
Ruth Mesfun <ruth@flatironschool.com>
2017-09-17 11:33:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,12 +26,14 @@ class Song
 
   def self.new_by_name(name)
     song = self.new
+    song.create
     song.name = name
     song
   end
 
   def self.create_by_name(name)
-    
+    self.class.create
+
   end
 
 end
