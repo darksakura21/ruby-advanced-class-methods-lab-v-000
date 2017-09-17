5c39a9d5a096b2db06e95c9ced05ce21f5a1f054
Ruth Mesfun <ruth@flatironschool.com>
2017-09-17 11:49:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) == true ? self.find_by_name(name) : self.create_by_name(name)
+    self.find_by_name(name) == true ? self.class.find_by_name(name) : self.create_by_name(name)
   end
 
 end
