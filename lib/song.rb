964f185a87868e4226f2b0bd5b6c719f4c282ce0
Ruth Mesfun <ruth@flatironschool.com>
2017-09-17 11:59:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
+    if self.find_by_name(name) == true
         self.all.each do |song|
           song.include?(name)
           song
